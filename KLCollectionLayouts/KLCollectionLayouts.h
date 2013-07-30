1ac3564d614b71369bb824458e6621b4db05c658
Kevin Lundberg <kevinrlundberg@gmail.com>
2013-07-30 09:56:07 -0400

    Initial Commit

---
+++ KLCollectionLayouts/KLCollectionLayouts.h
@@ -0,0 +1,13 @@
+//
+//  KLCollectionLayouts.h
+//  KLCollectionLayouts
+//
+//  Created by Kevin Lundberg on 7/30/13.
+//  Copyright (c) 2013 Kevin Lundberg. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KLCollectionLayouts : NSObject
+
+@end
---
+++ KLCollectionLayoutsTests/KLCollectionLayoutsTests.m
@@ -0,0 +1,36 @@
+//
+//  KLCollectionLayoutsTests.m
+//  KLCollectionLayoutsTests
+//
+//  Created by Kevin Lundberg on 7/30/13.
+//  Copyright (c) 2013 Kevin Lundberg. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface KLCollectionLayoutsTests : XCTestCase
+
+@end
+
+@implementation KLCollectionLayoutsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
